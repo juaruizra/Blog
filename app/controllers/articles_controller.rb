714dc71c9cdec5f25959a9e35d8b60adcf4bcee7
juaruizra <j_ruiz963@hotmail.com>
2016-08-02 01:55:37 -0500

    se actualiza contador y se implementan callback de control

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+  # before_action :validate_user, except: [:show,:index]
+  before_action :authenticate_user!, except: [:show,:index] #only: [:create,:new]
+  before_action :set_article, except: [:new,:index, :create]
+
   #A esta ruta se accede con el verbo
   #GET/articles
   def index
@@ -7,7 +11,8 @@ class ArticlesController < ApplicationController
 # -----------------------------------
 #GET/articles/:id
   def show
-    @article =Article.find(params[:id])
+    # @article =Article.find(params[:id])  #codigo igual
+    @article.update_visits_count
   end
   # -----------------------------------
   #GET/articles/new
@@ -17,7 +22,7 @@ class ArticlesController < ApplicationController
   end
   #POST/articles -> lo invoca al enviar formulario (dar clic en submit de new)
   def create
-    @article= Article.new(article_params)
+    @article= current_user.article.new(article_params)
     if @article.save
       redirect_to @article
     else
@@ -28,11 +33,11 @@ class ArticlesController < ApplicationController
   # GET /articles/:id/edit
       # ÈDITAR VA CON UPDATE
   def edit
-    @article= Article.find(params[:id])
+    # @article= Article.find(params[:id])  #codigo igual
   end
 
   def update
-      @article= Article.find(params[:id])
+      # @article= Article.find(params[:id])  #codigo igual
       if @article.update(article_params)
         redirect_to @article
       else
@@ -44,16 +49,23 @@ class ArticlesController < ApplicationController
 # -----------------------------------
   # DELETE  /articles/:id
   def destroy #le mandan del index
-    @article= Article.find(params[:id])
+    # @article= Article.find(params[:id]) #codigo igual
     @article.destroy
     redirect_to articles_path
   end
 # -----------------------------------
+
+# private
+# def validate_user
+#   redirect_to new_user_session_path, notice: "Hola,registrate"
+# end
+private
+def set_article
+  @article =Article.find(params[:id])
+end
 # ASPECTO DE SEGURIDAD, ES LO QUE EL USUARIO PUEDE EDITAR
+private
   def article_params
     params.require(:article).permit(:title,:body)
-
   end
-
-
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,18 @@ class Article < ActiveRecord::Base
   # la Tabla=> sabe la table por el nombre de la clase "Articles"
   # campos=> article.title()=> devuelve el titulo del darticulo
   # escribir metodos
-
+  belongs_to :user
   validates :title, presence: true
   validates :body, :presence => {:message => "Usted debe ingresar un título"}, length: {minimum: 2, maximum: 500, :message => "El título debe tener entre 2 y 500 caracteres"}
+
+  before_save :set_visits_count
+
+  def update_visits_count
+    self.save if self.visits_count.nil?
+    self.update(visits_count: self.visits_count + 1)
+  end
+  def set_visits_count
+    self.visits_count ||=0
+  end
+
 end
